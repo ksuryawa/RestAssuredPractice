f17d0a19b5aaaecd03e23171d4cd46d1af884944
ksuryawa <kapil.r.suryawanshi@gmail.com>
2022-06-08 20:20:09 +0100

    Add Get Request Call

---
+++ src/test/java/com/tests/GetRequest.java
@@ -0,0 +1,26 @@
+package com.tests;
+
+import io.restassured.http.Header;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+
+public class GetRequest{
+	@Test
+	public void getEmployees(){
+		Response response = given().get("http://localhost:3000/employees");
+
+		System.out.println(response.statusCode());
+		System.out.println(response.time());
+		System.out.println(response.contentType());
+
+		response.then().statusCode(HttpStatus.SC_OK);
+		for (Header header : response.headers()) {
+			System.out.println(header.getName() + " :" +header.getValue());
+		}
+
+
+		response.prettyPrint();
+	}
+}
